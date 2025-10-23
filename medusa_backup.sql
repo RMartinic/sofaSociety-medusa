2a7fd7fdf1c9066c02308723e9207f3823e043ae
RMartinic <roko.martinic2@gmail.com>
2025-10-24 00:37:49 +0200

    inital commit: added data to database

---
+++ medusa_backup.sql
@@ -0,0 +1,27 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict HjSqFkIj1jZAL1OBTj4eLGemBn1SkD76fbQX69P00K0NizZVqGm5ayU3WW0PCG0
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.6
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict HjSqFkIj1jZAL1OBTj4eLGemBn1SkD76fbQX69P00K0NizZVqGm5ayU3WW0PCG0
+
